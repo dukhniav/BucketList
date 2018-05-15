25c7cb175e08f3f31d661a06bcb39d6bb01494ad
Artem Dukhnitskiy <artem.dukhnitskiy@gmail.com>
2018-05-15 14:15:34 -0700

    update
    main
    floatingActionButton -> map
    
    gradle dependencies:
    
        implementation fileTree(dir: 'libs', include: ['*.jar'])
        implementation"org.jetbrains.kotlin:kotlin-stdlib-jre7:$kotlin_version"
        implementation 'com.android.support:appcompat-v7:26.1.0'
        implementation 'com.google.android.gms:play-services-maps:15.0.1'
        testImplementation 'junit:junit:4.12'
        androidTestImplementation 'com.android.support.test:runner:1.0.2'
        androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'
        compile 'com.android.support.constraint:constraint-layout:1.0.2'
        implementation 'com.android.support:cardview-v7:26.1.0'
        implementation 'com.android.support:recyclerview-v7:26.1.0'
        implementation "android.arch.persistence.room:runtime:1.0.0"
        annotationProcessor "android.arch.persistence.room:compiler:1.0.0"
        implementation 'com.android.support:design:26.1.0'

--- src/main/java/co/iamartem/visitbucketlist/MainActivity.kt
+++ src/main/java/co/iamartem/visitbucketlist/MainActivity.kt
@@ -1,10 +1,22 @@
 package co.iamartem.visitbucketlist
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by dukhnia on 5/10/18.
  */
 class MainActivity  : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        floatingActionButton.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- src/main/java/co/iamartem/visitbucketlist/RecyclerAdapter.kt
+++ src/main/java/co/iamartem/visitbucketlist/RecyclerAdapter.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.graphics.Color
 import android.icu.util.ULocale
 import android.os.Bundle
-import android.support.constraint.Group
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -18,10 +17,10 @@ import co.iamartem.visitbucketlist.R.id.*
 import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 import java.util.*
 
-class RecyclerAdapter(val bills : List<Location>) : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>(){
+class RecyclerAdapter(val locations : List<Location>) : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>(){
 
 
-    override fun getItemCount() = bills.size
+    override fun getItemCount() = locations.size
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerHolder{
         val layoutInflater = LayoutInflater.from(parent?.context)
@@ -31,41 +30,41 @@ class RecyclerAdapter(val bills : List<Location>) : RecyclerView.Adapter<Recycle
     }
 
     override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
-        val bill = bills.get(position)
+        val bill = locations.get(position)
 
-        holder.view.r_bill_name.text = bill.company
-        if (bill.amtPaid >= bill.amtDue) {
-            holder.view.r_amt_due?.text = ("Paid")
-            holder.view.r_amt_due.setTextColor(Color.GREEN)
-        } else {
-            holder.view.r_amt_due?.text = ("Amount Due: $ ${bill.amtDue.toString()}")
-        }
-        holder.view.r_due_date?.text = ("Date Due: ${bill.dueDate}")
-        holder.view.r_id?.text = (bill.id).toString()
+//        holder.view.r_bill_name.text = bill.company
+//        if (bill.amtPaid >= bill.amtDue) {
+//            holder.view.r_amt_due?.text = ("Paid")
+//            holder.view.r_amt_due.setTextColor(Color.GREEN)
+//        } else {
+//            holder.view.r_amt_due?.text = ("Amount Due: $ ${bill.amtDue.toString()}")
+//        }
+//        holder.view.r_due_date?.text = ("Date Due: ${bill.dueDate}")
+//        holder.view.r_id?.text = (bill.id).toString()
+//
+//        holder.view.full_row_id.setOnClickListener {
+//            Log.i("Tag","RecyclerView: Company A -> ${bill.company.toString()}")
+//            Log.i("Tag","RecyclerView: ExtraString A -> ${bill.id.toString()}")
+//
+//
+//            //fun onClick (view : View) {
+//            val intent = Intent(holder.view.full_row_id.getContext(), UpdateBill::class.java)
+//            intent.putExtra("id", bill.id.toString())
 
-        holder.view.full_row_id.setOnClickListener {
-            Log.i("Tag","RecyclerView: Company A -> ${bill.company.toString()}")
-            Log.i("Tag","RecyclerView: ExtraString A -> ${bill.id.toString()}")
-
-
-            //fun onClick (view : View) {
-            val intent = Intent(holder.view.full_row_id.getContext(), UpdateBill::class.java)
-            intent.putExtra("id", bill.id.toString())
-
-            var bun = Bundle()
-            bun.putBoolean("isActive", true)
-            intent.putExtras(bun)
-
-            holder.view.full_row_id.getContext().startActivity(intent)
+//            var bun = Bundle()
+//            bun.putBoolean("isActive", true)
+//            intent.putExtras(bun)
+//
+//            holder.view.full_row_id.getContext().startActivity(intent)
             //}
 //
 //            val intent = Intent(this, UpdateBill::class.java)
 //            startActivity(intent)
 //            //UpdateBill().loadBill(bill.id.toString())
 
-            Log.i("Tag","RecyclerView: Company B -> ${bill.company.toString()}")
+            Log.i("Tag","RecyclerView: Company B -> ${locations.toString()}")
 
-        }
+        //}
     }
 
     inner class RecyclerHolder(val view : View) : RecyclerView.ViewHolder(view) {
